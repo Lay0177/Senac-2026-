cded332da67451154581b395c2abc86af2ef2431
Layane Raíssa <layaneraissatabletinfo@gmail.com>
2026-02-25 14:40:47 -0300

    Add files via upload

---
+++ UC-algoritmo (25.02) python/tipagem.py
@@ -0,0 +1,12 @@
+nome = "Ada"
+print(type(nome))
+
+idade = 10 
+print (type(idade))
+
+nota = 9.99
+print(type(nota))
+
+exemplo = True
+print(type(exemplo))
+
---
+++ UC-algoritmo (25.02) python/variaveis.py
@@ -0,0 +1,8 @@
+var1=10
+Var1=20
+
+
+print(var1)
+print(Var1)
+
+#Foram duas variáveis, mesmo estando iguais, a primeira letra estando diferente também diferencia a variável. Nesse caso irá imprimir os dois.
