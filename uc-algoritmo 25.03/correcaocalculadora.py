946d66bcc5c25ccee970b0214336690eea7a5970
Layane Raíssa <layaneraissatabletinfo@gmail.com>
2026-03-25 14:35:01 -0300

    Add files via upload

---
+++ uc-algoritmo 25.03/correcaocalculadora.py
@@ -0,0 +1,35 @@
+def calculadora():
+    print("=== CALCULADORA ===")
+    print("1. Soma")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+    print("5. Sair")
+
+    while True:
+    opcao = input("/N Escolha uma operação 1-5): ")
+
+    if opcao == "S":
+        print("Até logo!")
+        break
+
+    if opcao in ["1","2","3","4"]:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 =float(input("Digite o segundo número: "))
+
+        if opcao == "1":
+            print (f"Resultado: {soma(num1, num2)}")
+
+        elif opcao == "2":
+            print (f"Resultado: {subtracao(num1, num2)}")
+
+        elif opcao == "3":
+            print (f"Resultado: {multiplicacao(num1, num2)}")
+
+        elif opcao == "3":
+            print (f"Resultado: {divisao(num1, num2)}")
+
+        
+
+
+        
