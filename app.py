6bab04d769648111713bf6b5ad723146881965f2
Layane Raíssa <layaneraissatabletinfo@gmail.com>
2026-05-07 14:42:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,50 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/Calabresa')
+def pizzaria():
+    return render_template('Calabresa.html')
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Layane"
-    return render_template('index.html', title='Página Inicial', usuario=None, 
-    nome=None, title= 'Home')
-
-@app.route('/usuario')
-def usuario():
-    usuario = {'nome': 'Layane', 'email': 'layanerai900@gmail.com'}
-    return render_template('index.html', title='Página do Usuário', usuario=usuario, nome=None)
-
-@app.route('/dados', defaults = {"nome": "usuário comum"})
-@app.route('/dados/<nome>')
-def dados(nome):
-    return f'Olá, {nome}!'
-
-@app.route('/semestre/<int:x>')
-def semestre(x):
-    return f'Você está no semestre' + str(x)
-
-@app.route('/pagamento/<float:valor>')
-def pagamento(valor):
-    return f'Você pagou: '+ str(valor)
-
-@app.route('/somar', defaults={"n1": 0, "n2": 0})
-@app.route('/somar/<int:n1>/<int:n2>')
-def somar(n1, n2):
-    resultado = n1 + n2
-    return str(resultado)
-
-@app.route('/soma', defaults={"n1": 0, "n2": 0})
-@app.route('/somar/<int:n1>/<int:n2>')
-def somar(n1, n2):
-    resultado = n1 + n2
-    return render_template ('somar.html', n1=n1, n2=n2, resultado=resultado)
-
-@app.route('/arearestrita/<int:id>')
-def arearestrita(id):
-  if id == 1:
-     return "Acesso bloqueado (cadeado fechado)"
-  else:
-      return "Acesso liberado (cadeado aberto)"
+@app.route('/Frango')
+def pizzaria():
+    return render_template('Frango com catupiry.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
